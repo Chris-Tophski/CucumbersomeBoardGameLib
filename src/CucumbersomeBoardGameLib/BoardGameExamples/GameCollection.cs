51f55d9989cdcdf912ecaf33def7c255ee31c362
Chris-Tophski <62284362+Chris-Tophski@users.noreply.github.com>
2025-08-23 17:12:08 +0200

    added game collection impl

---
+++ src/CucumbersomeBoardGameLib/BoardGameExamples/GameCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CucumbersomeBoardGameLib.Games;
+
+namespace BoardGameExamples
+{
+	public class GameCollection
+		: GameCollectionBase
+	{
+		
+	}
+}
