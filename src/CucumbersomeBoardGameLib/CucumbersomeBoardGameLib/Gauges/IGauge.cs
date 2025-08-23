8dca9c453ef45ebf4a1312c809a5c7274605cda6
Chris-Tophski <62284362+Chris-Tophski@users.noreply.github.com>
2025-08-23 15:35:25 +0200

    added gauges and wheels with code docs

---
+++ src/CucumbersomeBoardGameLib/CucumbersomeBoardGameLib/Gauges/IGauge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CucumbersomeBoardGameLib.Gauges
+{
+	/// <summary>
+	/// Representation of a single gauge in a game.
+	/// The gauge may represent a value specific for the entire session
+	/// or some level definition of a player etc.
+	/// </summary>
+	public interface IGauge
+	{
+
+	}
+}
---
+++ src/CucumbersomeBoardGameLib/CucumbersomeBoardGameLib/Wheels/IWheel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CucumbersomeBoardGameLib.Wheels
+{
+	/// <summary>
+	/// Representation of a single wheel in a game,
+	/// used as an alternative to dice.
+	/// </summary>
+	public interface IWheel
+	{
+
+	}
+}
